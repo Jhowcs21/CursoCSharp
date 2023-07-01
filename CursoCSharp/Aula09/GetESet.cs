6ffdb222523d30605c3dcd7e7d028c603b195f58
Jhowcs21 <113648222+Jhowcs21@users.noreply.github.com>
2023-07-01 11:55:32 -0300

    Add files via upload

---
+++ CursoCSharp/Aula09/GetESet.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Aula09
+{
+    class GetESet
+    {
+        private string Nome;
+        private int Idade;
+       
+
+        public GetESet(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public GetESet()
+        {
+
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            Nome = nome;
+        }
+            
+        public int GetIdade()
+        {
+
+            return Idade = Math.Abs(Idade);
+        }
+
+        public void SetIdade(int idade)
+        {
+
+            Idade = Math.Abs(Idade);
+            Idade = idade;
+            
+        }
+
+
+        class obj
+        {
+            public static void Main()
+            {
+                GetESet pessoal = new GetESet("Jonatha", -30);
+                Console.WriteLine(pessoal.GetNome());
+                Console.WriteLine(pessoal.GetIdade());
+
+                var Pessoa02 = new GetESet();
+                Pessoa02.SetNome("Joao");
+                Pessoa02.SetIdade(-22);
+                Console.WriteLine(Pessoa02.GetNome());
+                Console.WriteLine(Pessoa02.GetIdade());
+            }
+        }
+
+    }
+}
---
+++ CursoCSharp/Aula09/Params.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Aula09
+{
+    internal class Params
+    {
+        /*
+        public static void Apresentar(params string[] alunos)
+        {
+            foreach(string aluno in alunos)
+            {
+                Console.WriteLine($"Olá, {aluno}! Bom dia!");
+            }
+        }
+        public static void Main()
+        {
+            Apresentar("Jonatha", "Gabriel", "Gabriel2", "Nathalia", "Ligia");
+        */
+        public static void Apresentar(params string[] marcas)
+        {
+            foreach(string marca in marcas)
+            {
+                Console.WriteLine($"A marca: {marca}, é uma das suas favoritas");
+            }
+        }
+
+        public static void Main241574()
+        {
+            Apresentar($"Lacoste", "Nike", "Mizuno", "Oakley");
+        }
+        
+    }
+}
